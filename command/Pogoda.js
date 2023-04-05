79b1221a3193420b326ec76a6417b3cb9f84c140
xanax <34755589+anKordii@users.noreply.github.com>
2023-04-05 19:50:25 +0200

    Update Pogoda.js

--- command/Pogoda.js
+++ command/Pogoda.js
@@ -13,7 +13,7 @@ export default async function commandPogoda(user, argumentClean, cooldown) {
     (weatherData && weatherData.current_condition.length === 0) ||
     !weatherData.nearest_area
   ) {
-    return `${userName}, nie udało sie pobrać danych pogodowych - WTTR.`;
+    return `${user}, nie udało sie pobrać danych pogodowych - WTTR.`;
   }
   const { current_condition, nearest_area } = weatherData;
   const pogoda = `${user} w ${nearest_area[0].areaName[0].value} aktualnie jest ${current_condition[0].FeelsLikeC} stopni, na zewnątrz jest ${current_condition[0].lang_pl[0].value} a prędkość wiatru wynosi ${current_condition[0].windspeedKmph} km/h`;

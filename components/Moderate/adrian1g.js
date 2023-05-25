f00068653dd9592bdadf9356f2acda518a08f4a3
xanax <34755589+anKordii@users.noreply.github.com>
2023-05-25 18:01:01 +0200

    Update adrian1g.js

--- components/Moderate/adrian1g.js
+++ components/Moderate/adrian1g.js
@@ -1,7 +1,7 @@
 export default async function moderate1G(msg, api, user){
     const args = msg.split(" ");
 
-    if(args.includes("gg/jasper")){
+    if(args.includes("kingfvxq")){
       const getChannelID = await api.users
       .getUserByName(user)
       .catch((e) => {

a7401bc9b64cb020da4080a3f148741775672f78
xanax <34755589+anKordii@users.noreply.github.com>
2023-06-14 02:26:53 +0200

    neex

--- components/TTS/Redeem.js
+++ components/TTS/Redeem.js
@@ -17,7 +17,7 @@ export default async function redeemTTS(channel, user, msg, tags, api, io, free)
       return null;
     }
 
-    if(channelClean === "adrian1g__" || free === true && channel !== "grubamruwa"){
+    if(channelClean === "adrian1g__" || free === true && !["neexcsgo", "grubamruwa"].includes(channel)){
 
       io.emit( "new-free-tts", {
         msg: msg,
